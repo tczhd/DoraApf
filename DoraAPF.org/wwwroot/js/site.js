13c4364fc35a134971d1ebcf2d1a368b31d7bc0b
david <hongdingzhu@hotmail.com>
2019-05-21 23:47:59 -0700

    Check in code

--- DoraAPF.org/wwwroot/js/site.js
+++ DoraAPF.org/wwwroot/js/site.js
@@ -1,4 +1,38 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+    var removeActive = function () {
+        $(".doraapf-navbar .nav a").parents("li, ul").removeClass("active");
+    };
+
+    $(".doraapf-navbar .nav li").click(function () {
+        removeActive();
+        $(this).addClass("active");
+    });
+
+    removeActive();
+    var parent = $(".doraapf-navbar a[href='" + location.pathname + "']").parent("li");
+    parent.addClass("active");
+
+    var secondParent = parent.parent().parent("li");
+    if (secondParent) {
+        secondParent.addClass("active");
+    }
+
+    //var changeLoginPosition = function () {
+    //    var navLoginSection = $('#navLoginSection');
+
+    //    if ($(window).width() <= 767) {
+
+    //        navLoginSection.removeClass('pull-right');
+    //    }
+    //    else {
+    //        navLoginSection.addClass('pull-right');
+    //    }
+    //};
+
+    //$(window).resize(function () {
+    //    changeLoginPosition();
+    //});
+
+    //changeLoginPosition();
+});
