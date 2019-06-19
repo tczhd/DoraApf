7000338d22b748bda22b13965d7a2bcf79a9dbf7
hongdingzhu@hotmail.com <David@orderbot.com>
2019-06-19 13:59:58 -0700

    Check in payment code

---
+++ DoraAPF.org/Models/DoraEnums.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace DoraAPF.org.Models
+{
+    public enum Title
+    {
+        [Description("Mr. ")]
+        Mr = 1,
+        [Description("Mrs. ")]
+        Mrs = 2,
+        [Description("MS. ")]
+        Ms = 3,
+        [Description("Miss. ")]
+        Miss = 4
+    }
+}
---
+++ DoraAPF.org/Models/ListItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoraAPF.org.Models
+{
+    public class ListItemModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
