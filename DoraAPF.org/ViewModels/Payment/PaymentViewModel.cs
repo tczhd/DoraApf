c5f1dcb8ce460c7855586b1f4a4d452cdff7ef18
david <hongdingzhu@hotmail.com>
2019-06-18 23:55:37 -0700

    Check in code

--- DoraAPF.org/ViewModels/Payment/PaymentViewModel.cs
+++ DoraAPF.org/ViewModels/Payment/PaymentViewModel.cs
@@ -4,6 +4,28 @@ namespace DoraAPF.org.ViewModels.Payment
 {
     public class PaymentViewModel
     {
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Address1")]
+        public string Address1 { get; set; }
+        [Display(Name = "Address2")]
+        public string Address2 { get; set; }
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Display(Name = "State")]
+        public string State { get; set; }
+        [Display(Name = "Country")]
+        public string County { get; set; }
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
         [Display(Name = "Donation Amount")]
         public string PaymentAmount { get; set; }
         [Display(Name = "Card Holder Name")]
